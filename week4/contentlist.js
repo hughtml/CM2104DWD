ff4b8ddaac2f49c1b59836cc0311008135328909
hughtml <1600644@rgu.ac.uk>
2018-02-19 22:39:46 +0000

    Doing it through elements

--- week4/contentlist.js
+++ week4/contentlist.js
@@ -3,16 +3,24 @@ function addContent () {
 	// add a list of items to the content div
 	var items = ["hewey", "dewey", "louie"];
 
-	// build the html string for a <ul> list
-	var items_html = "<ul>";
+	/* var items_html = "<ul>";
 	for (var i=0; i < items.length; i++) {
 		item = items[i];
 		items_html += "<li>" + item + "</li>";
 	};
-	items_html += "</ul>";
+	items_html += "</ul>"; */
+
+	//var items_html;
+	for (var i=0; i < items.length; i++) {
+		var newElement = document.createElement("p");
+		var itemText = items[i];
+		newElement.appendChild(itemText);
+		document.body.appendChild(newPara);
+	};
+	//items_html += "</ul>";
 
 	// using javascript
 	// 1. find the content div
 	// 2. modify its html attribute by adding items_html
-	document.getElementById("content").innerHTML = items_html;
+	//document.getElementById("content").innerHTML = items_html;
 }
