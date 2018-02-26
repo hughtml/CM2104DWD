1c1cc0631aeafa774a9f6279c70ab3dbafd45ec2
hughtml <1600644@rgu.ac.uk>
2018-02-26 21:47:09 +0000

    Test

--- week4/catspin.js
+++ week4/catspin.js
@@ -9,9 +9,9 @@ function animate(time) {
         angle += (time - lastTime) * 0.001;
     lastTime = time;
     cat.style.top = (Math.sin(angle) * 20) + "px";
+    console.log(cat.style.top);
     cat.style.left = (Math.cos(angle) * 200) + "px";
+    console.log(cat.style.left);
     requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
-
-
