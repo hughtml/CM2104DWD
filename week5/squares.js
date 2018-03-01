87846193fbd0ebc7849a7dc0e0517fe151ff1b27
hughtml <1600644@rgu.ac.uk>
2018-03-01 09:27:53 +0000

    Testing effects.

--- week5/squares.js
+++ week5/squares.js
@@ -9,10 +9,14 @@ $(function(){
 		// $(this) is a shortcut for the element we just selected
 		// using $("div")
 		if ($(this).hasClass("red")) {
+			$(this).fadeOut();
 			$(this).toggleClass("blue");
+			$(this).fadeIn();
 		}
 		else if ($(this).hasClass("blue")) {
+			$(this).slideUp("slow", function() {});
 			$(this).toggleClass("green");
+			$(this).slideDown("slow", function() {});
 		}
 		else if ($(this).hasClass("green")) {
 			$(this).toggleClass("red");
