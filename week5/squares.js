3fd0eca79444b7b7307e70ade5697d40e156d72e
hughtml <1600644@rgu.ac.uk>
2018-03-01 09:31:19 +0000

    Test

--- week5/squares.js
+++ week5/squares.js
@@ -14,8 +14,9 @@ $(function(){
 			$(this).fadeIn();
 		}
 		else if ($(this).hasClass("blue")) {
-			//$(this).slideUp("slow", function() {});
 			$(this).toggleClass("green");
+			//$(this).slideUp("slow", function() {});
+
 			//$(this).slideDown("slow", function() {});
 		}
 		else if ($(this).hasClass("green")) {
