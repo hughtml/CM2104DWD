c3fdbbdc5aa9a51b7d3561593a3332f5dcec595d
hughtml <1600644@rgu.ac.uk>
2018-03-05 15:25:27 +0000

    Started task

---
+++ week5/table.js
@@ -0,0 +1,12 @@
+$(function(){
+	//Starting point for when the page loads
+
+  var rows = $("tr");
+  //Storing all the rows in an array
+
+  for (i = 0; i < rows.length; i++) {
+      rows[i]
+  }
+  //Iterating through the array, changing every other row colour
+
+});
