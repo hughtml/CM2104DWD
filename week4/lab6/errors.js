c05d31696e33d6983802c42004abb33d63db7237
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-03-30 18:25:29 +0100

    Fixing first errors - comments included.

--- week4/lab6/errors.js
+++ week4/lab6/errors.js
@@ -1,12 +1,13 @@
 function main () {
-	var a b
+	var a, b;
+	//A semi-colon was missing, causing the Console find error with this line
 	var c;
-	
+
 	a = 10;
 	b = 100;
-	
+
 	b = + a b;
 	c == a * b;
-	
+
 	document.writeln(c);
 }
