be4c1bcf5a6a84d666e13f9450f5749bc4d567b0
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-02 15:38:39 +0100

    Adding comments explaining error.

--- week4/lab6/errors.js
+++ week4/lab6/errors.js
@@ -9,6 +9,8 @@ function main () {
 	b = a + b;
 	//By swapping the + operator and variable a, the error of "unexpected identifier" has been rectified
 	c == a * b;
+	//As this is syntactically correct, it does not display an error
+	//However, this is logically incorrect as "==" will create a Boolean statement of "is c equal to the result of a * b?" aka false, therefore leaving c as undefined
 
 	document.writeln(c);
 }
