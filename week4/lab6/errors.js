d02a4485969e2677baa30678c89bf713b6d68599
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-02 15:39:12 +0100

    Changed code to be logically correct.

--- week4/lab6/errors.js
+++ week4/lab6/errors.js
@@ -8,9 +8,8 @@ function main () {
 
 	b = a + b;
 	//By swapping the + operator and variable a, the error of "unexpected identifier" has been rectified
-	c == a * b;
-	//As this is syntactically correct, it does not display an error
-	//However, this is logically incorrect as "==" will create a Boolean statement of "is c equal to the result of a * b?" aka false, therefore leaving c as undefined
+	c = a * b;
+	//Changing the code to be logically correct
 
 	document.writeln(c);
 }
