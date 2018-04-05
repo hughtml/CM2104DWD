4c1e49ebb95f596d6f2a07639d873763758a70ec
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-05 13:46:54 +0100

    Completing the JavaScript to add ducks to the inner HTML.

--- week4/lab6/contentlist.js
+++ week4/lab6/contentlist.js
@@ -2,7 +2,7 @@
 function addContent () {
 	// add a list of items to the content div
 	var items = ["hewey", "dewey", "louie"];
-	
+
 	// build the html string for a <ul> list
 	var items_html = "<ul>";
 	for (var i=0; i < items.length; i++) {
@@ -10,10 +10,14 @@ function addContent () {
 		items_html += "<li>" + item + "</li>";
 	};
 	items_html += "</ul>";
-	
+
 	// using javascript
 	// 1. find the content div
 	// 2. modify its html attribute by adding items_html
 
-}
+	var contentDiv = document.getElementById("content");
+	//Finding the content div
+	contentDiv.innerHTML = items_html;
+	//Modifying its inner HTML, adding the items_html variable
 
+}
