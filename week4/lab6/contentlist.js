b0d4430dadca6d59effc60aaf8bcb5243d5a49eb
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-05 14:01:27 +0100

    Forgot to incase everything in a method.

--- week4/lab6/contentlist.js
+++ week4/lab6/contentlist.js
@@ -1,42 +1,46 @@
 
-function addContent () {
-	// add a list of items to the content div
-	var items = ["hewey", "dewey", "louie"];
+function() {
 
-	// build the html string for a <ul> list
-	var items_html = "<ul>";
-	for (var i=0; i < items.length; i++) {
-		item = items[i];
-		items_html += "<li>" + item + "</li>";
-	};
-	items_html += "</ul>";
+	function addContent () {
+		// add a list of items to the content div
+		var items = ["hewey", "dewey", "louie"];
 
-	// using javascript
-	// 1. find the content div
-	// 2. modify its html attribute by adding items_html
+		// build the html string for a <ul> list
+		var items_html = "<ul>";
+		for (var i=0; i < items.length; i++) {
+			item = items[i];
+			items_html += "<li>" + item + "</li>";
+		};
+		items_html += "</ul>";
 
-	var contentDiv = document.getElementById("content");
-	//Finding the content div
-	contentDiv.innerHTML = items_html;
-	//Modifying its inner HTML, adding the items_html variable
+		// using javascript
+		// 1. find the content div
+		// 2. modify its html attribute by adding items_html
 
-}
+		var contentDiv = document.getElementById("content");
+		//Finding the content div
+		contentDiv.innerHTML = items_html;
+		//Modifying its inner HTML, adding the items_html variable
 
-function addContentElements() {
-	var items = ["Hewey", "Dewey", "Louie"];
-	//Creating an array to store the items
-
-	var list = document.createElement("ul");
-	//Creating the unordered list element
-	for (var i = 0; i < items.length; i++) {
-		var newContent = document.createElement("li");
-		//Creating a new list item element
-		newContent.appendChild(document.createTextNode(items[i]));
-		//Creating a new text node using the current item in the array and adding it to the list item element
 	}
-	//Iterating through the whole array
-	var contentDiv = document.getElementById("content");
-	//Finding the content div
-	contentDiv.appendChild(list);
-	//Adding the list
+
+	function addContentElements() {
+		var items = ["Hewey", "Dewey", "Louie"];
+		//Creating an array to store the items
+
+		var list = document.createElement("ul");
+		//Creating the unordered list element
+		for (var i = 0; i < items.length; i++) {
+			var newContent = document.createElement("li");
+			//Creating a new list item element
+			newContent.appendChild(document.createTextNode(items[i]));
+			//Creating a new text node using the current item in the array and adding it to the list item element
+		}
+		//Iterating through the whole array
+		var contentDiv = document.getElementById("content");
+		//Finding the content div
+		contentDiv.appendChild(list);
+		//Adding the list
+	}
+
 }
