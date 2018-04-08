d09696b679af0db694bfb8e3af5b12ce625bf17f
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-08 20:02:29 +0100

    Simplifying rest of if/else.

--- week5/lab9/squares.js
+++ week5/lab9/squares.js
@@ -10,12 +10,15 @@ $(function(){
 		// using $("div")
 		if ($(this).hasClass("red")) {
 			$(this).toggleClass("blue red");
+			//Simplifying the if/else statement with a toggleClass method
 		}
 		else if ($(this).hasClass("blue")) {
-			$(this).addClass("green").removeClass("blue");
+			$(this).toggleClass("green blue");
+			//Simplifying the if/else statement with a toggleClass method
 		}
 		else if ($(this).hasClass("green")) {
-			$(this).addClass("red").removeClass("green");
+			$(this).toggleClass("red green");
+			//Simplifying the if/else statement with a toggleClass method
 		}
 	});
 
