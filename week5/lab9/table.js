4e508529386179c49aa7ae8f37227ef931d5f1e4
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-08 21:21:21 +0100

    Adding the JQuery and JavaScript code.

---
+++ week5/lab9/table.js
@@ -0,0 +1,14 @@
+$(function(){
+//Function running when the document loads
+
+    var rows = $(tr);
+    //Storing all the table rows in an array
+    for (int i = 0; i < rows.length; i++) {
+        if ((i == 0) || (i % 2 == 0) {
+            rows[i].addClass("row2");
+        } else {
+            rows[i].addClass("row1");
+        }
+    } //Iterating through all of the rows in the table
+
+});
