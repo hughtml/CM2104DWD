9a705e81c616e4bd29b5856a5174baf2f58613c8
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-08 16:58:09 +0100

    Putting code back.

--- week5/lab9/contentlist.js
+++ week5/lab9/contentlist.js
@@ -7,30 +7,30 @@ $(function(){
 	// 2. add an event listener for 'click' events
 	// 3. assign the addContent() function to the click event
 
-	$('#addcontent').bind("click", addContent());
+	$('#addcontent').click(addContent());
 	//Finding the addcontent button, assigning an event listener for click events
 	//The addContent() function is the handler for these events
 
-	function addContent () {
-		// add a list of items to the content div
-		var items = ["hewey", "dewey", "louie"];
-
-		// build the html string for a <ul> list
-		var items_html = "<ul>";
-		for (var i=0; i < items.length; i++) {
-			item = items[i];
-			items_html += "<li>" + item + "</li>";
-		};
-		items_html += "</ul>";
-
-		// using jQuery
-		// 1. find the content div
-		// 2. modify its html attribute by adding items_html
-
-		$('#content').html(items_html);
-		//Finding the content div
-		//The inner HTML is then modified, adding items_html
-
-	}
-
 });
+
+function addContent () {
+	// add a list of items to the content div
+	var items = ["hewey", "dewey", "louie"];
+
+	// build the html string for a <ul> list
+	var items_html = "<ul>";
+	for (var i=0; i < items.length; i++) {
+		item = items[i];
+		items_html += "<li>" + item + "</li>";
+	};
+	items_html += "</ul>";
+
+	// using jQuery
+	// 1. find the content div
+	// 2. modify its html attribute by adding items_html
+
+	$('#content').html(items_html);
+	//Finding the content div
+	//The inner HTML is then modified, adding items_html
+
+}
