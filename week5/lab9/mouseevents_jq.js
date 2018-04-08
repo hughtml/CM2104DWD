db3d5549c945d2aba3b8b3136fafe72c26377301
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-08 19:31:08 +0100

    ...forgot the #.

--- week5/lab9/mouseevents_jq.js
+++ week5/lab9/mouseevents_jq.js
@@ -14,22 +14,22 @@ $(function(){
 	// now you need to do the same for button1 for mouseleave
 	// and mouseup, mousedown for button2
 
-	$("button1").mouseleave(function() {
-		$("button1").html("<p>Mouse Over Me</p>");
+	$("#button1").mouseleave(function() {
+		$("#button1").html("<p>Mouse Over Me</p>");
 		//Editing the inner HTML of the first buttsoon
 	});
 	//Finding button1
 
-	$("button2").mousedown(function() {
-		$("button2").html("<p>Release Me</p>");
-		$("button2").css("backgroundColor", "lightblue");
+	$("#button2").mousedown(function() {
+		$("#button2").html("<p>Release Me</p>");
+		$("#button2").css("backgroundColor", "lightblue");
 		//Editing the inner HTML and colour of the second buttoon
 	});
 	//Finding button2
 
-	$("button2").mouseup(function() {
-		$("button2").html("<p>Thank You</p>");
-		$("button2").css("backgroundColor", "lightgreen");
+	$("#button2").mouseup(function() {
+		$("#button2").html("<p>Thank You</p>");
+		$("#button2").css("backgroundColor", "lightgreen");
 		//Editing the inner HTML and colour of the second buttoon
 	});
 	//Finding button2
