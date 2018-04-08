4a8dee7e81038984b7498828c002965594fe5ab7
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-08 19:09:45 +0100

    Added comments to show understanding of code.

--- week5/lab9/mouseevents.js
+++ week5/lab9/mouseevents.js
@@ -1,18 +1,24 @@
 // JavaScript Document
 function mOver(obj) {
-    obj.innerHTML = "Thank You"
+    obj.innerHTML = "Thank You";
+    //Changing the text within the button when the mouse hovers over
 }
 
 function mOut(obj) {
-    obj.innerHTML = "Mouse Over Me"
+    obj.innerHTML = "Mouse Over Me";
+    //Changing the text within the button when the mouse moves away
 }
 
 function mDown(obj) {
     obj.style.backgroundColor = "lightblue";
     obj.innerHTML = "Release Me";
+    //When the button is clicked on, or the mouse button is held down on it, the colour is changed
+    //The text within the button is also changed
 }
 
 function mUp(obj) {
     obj.style.backgroundColor="lightgreen";
     obj.innerHTML="Thank You";
+    //When the button is clicked on, or the mouse button is release on it, the colour is changed
+    //The text within the button is also changed
 }
