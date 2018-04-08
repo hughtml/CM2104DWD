307c8b504bb489b6be9e7fb2afcaf2b60682b70b
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-08 20:24:06 +0100

    Testing slideDown and slideUp.

--- week5/lab9/squares.js
+++ week5/lab9/squares.js
@@ -9,16 +9,20 @@ $(function(){
 		// $(this) is a shortcut for the element we just selected
 		// using $("div")
 		if ($(this).hasClass("red")) {
-			$(this).fadeOut(800);
-			$(this).delay(400);
 			$(this).toggleClass("blue red");
 			//Simplifying the if/else statement with a toggleClass method
+			$(this).fadeOut(800);
+			$(this).delay(400);
 			$(this).fadeIn(800);
-			//When this square it clicked, it will fade out then change it's colour and fade in
+			//When this square it clicked, it will change it's colour then fade out and fade in
 		}
 		else if ($(this).hasClass("blue")) {
 			$(this).toggleClass("green blue");
 			//Simplifying the if/else statement with a toggleClass method
+			$(this).slideDown(800);
+			$(this).delay(400);
+			$(this).slideUp(800);
+			//When this square it clicked, it will change it's colour then slide down and slide up
 		}
 		else if ($(this).hasClass("green")) {
 			$(this).toggleClass("red green");
