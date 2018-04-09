1d7c18db49910fe3f04caca2bfa1b1cd24a82409
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-09 10:01:35 +0100

    Adding code to load the resource.

--- week5/lab10/getresources.js
+++ week5/lab10/getresources.js
@@ -3,6 +3,21 @@ $(document).ready(function() {
 
     $('#retrieve-resources').click(function( {
 
+      var displayResources = $('#display-resources');
+      //Setting up the page component to write to
+      displayResources.text('Loading data from JSON source...');
+      //Giving it an initial value
+
+      $.ajax({
+        type: "GET",
+        //The type of AJAX request - a simple GET
+        url: "resources.json",
+        //A URL which is the source of the JSON file
+        success: function(result) {
+            //Data will be dealt with here
+        } //Success function where the code will go when the file is successfully loaded
+      })
+
     }));
     //Adding a handler for the click event for a retrieve-resources button
 
