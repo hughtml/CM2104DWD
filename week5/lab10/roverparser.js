adb52e370e0ce41c1396afd5497bdba18f4b1aeb
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-09 12:08:24 +0100

    Adding earth_date to the table.

--- week5/lab10/roverparser.js
+++ week5/lab10/roverparser.js
@@ -8,31 +8,27 @@
  $(document).ready(function () {
 
   $('#retrieve-data').click(function () {
-  var displayResources = $('#display-data');
+    var displayResources = $('#display-data');
 
-  displayResources.text('Loading Rover Data');
+    displayResources.text('Loading Rover Data');
 
-  $.ajax({
-  type: "GET",
-  url: "roverdata.json",
-  success: function(result)
-  {
-  console.log(result.photos);
-  var photos = result.photos //this data contains an array called photos
+    $.ajax({
+      type: "GET",
+      url: "roverdata.json",
+      success: function(result) {
+        console.log(result.photos);
+        var photos = result.photos //this data contains an array called photos
 
-  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th></thead><tbody>";
-  for (var i in photos)
-  {
-    output+="<tr><td>" + photos[i].rover.name + "</td><td>"
-    + photos[i].camera.full_name + "</td><td>"
-    + photos[i].img_src + "</td></tr>";
-  }
-  output+="</tbody></table>";
+        var output = "<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>Earth Date</th></thead><tbody>";
+        for (var i in photos)  {
+          output+="<tr><td>" + photos[i].rover.name + "</td><td>" + photos[i].camera.full_name + "</td><td>" + photos[i].img_src + photos[i].earth_date + "</td></tr>";
+        }
+        output+="</tbody></table>";
 
-  displayResources.html(output);
-  $("table").addClass("table");
-  }
-  });
+        displayResources.html(output);
+        $("table").addClass("table");
+      }
+    });
 
   });
  });
