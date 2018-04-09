064ad112f19a3f842ae568da6df9b08adac59615
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-09 09:36:25 +0100

    Commit.

--- week5/lab9/table.js
+++ week5/lab9/table.js
@@ -3,12 +3,33 @@ $(function(){
 
     var rows = $('tr');
     //Storing all the table rows in an array
+
+    multicolor();
+    //Calling the multicolor function
+
     for (var i = 0; i < rows.length; i++) {
-        if ((i == 0) || (i % 2 == 0)) {
-            $(rows[i]).addClass("row2");
-        } else {
-            $(rows[i]).addClass("row1");
-        }
+
+        $(rows[i]).mouseenter($(rows[i]).removeClass("row1 row2").addClass("rowhighlighted"););
+        //Adding an event listener for when the row needs to be highlighted
+        $(rows[i]).mouseleave($(rows[i]).removeClass("rowhighlighted").addClass);
+        //Adding an event listener for when the row needs to be unhighlighted
+
     } //Iterating through all of the rows in the table
 
 });
+
+function multicolor() {
+
+  for (var i = 0; i < rows.length; i++) {
+
+      //------ ALTERNATING COLOURS ------
+
+      if ((i == 0) || (i % 2 == 0)) {
+          $(rows[i]).addClass("row2");
+      } else {
+          $(rows[i]).addClass("row1");
+      }
+
+  } //Iterating through all of the rows in the table
+
+}
