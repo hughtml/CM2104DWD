0f3757873e3fb4bb55e8a743679266094dcff7d7
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-09 09:50:23 +0100

    Creating initial code i.e. adding user interaction.

---
+++ week5/lab10/getresources.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+//Running when the page is loaded
+
+    $('#retrieve-resources').click(function( {
+
+    }));
+    //Adding a handler for the click event for a retrieve-resources button
+
+})
