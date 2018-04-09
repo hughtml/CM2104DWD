30d94267ff55ea12af7fcb91dbfa8caf73d5accd
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-09 10:21:50 +0100

    Final tweaks.

--- week5/lab10/getresources.js
+++ week5/lab10/getresources.js
@@ -20,12 +20,17 @@ $(document).ready(function() {
             //Creating a String with HTML representing table headers
 
             for (var i in result) {
-
               output += "<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
               //Asking each result's JSON for the name, provider and url
               //Surrounding each of these in <td> tags to continue formatting
-
             } //Running through each JSON result
+            output += "</tbody></table>";
+            //Closing the table tags
+
+            displayResources.html(output);
+            //Telling the displayResources to have the output String as its content
+            $("table").addClass("table");
+            //Adding the table class to the table for styling
 
         } //Success function where the code will go when the file is successfully loaded
       })
