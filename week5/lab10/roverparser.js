81caa37890951d4cfc6b614cb7480c5221b2c4a4
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-09 12:18:11 +0100

    Forgot column header.

--- week5/lab10/roverparser.js
+++ week5/lab10/roverparser.js
@@ -19,7 +19,7 @@
         console.log(result.photos);
         var photos = result.photos //this data contains an array called photos
 
-        var output = "<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>Earth Date</th></thead><tbody>";
+        var output = "<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>Earth Date</th><th>Launch Date</th></thead><tbody>";
         for (var i in photos)  {
           output+="<tr><td>" + photos[i].rover.name + "</td><td>" + photos[i].camera.full_name + "</td><td><img src='" + photos[i].img_src + "'></td><td>" + photos[i].earth_date + "</td><td>" + photos[i].rover.launch_date + "</td></tr>";
         }
