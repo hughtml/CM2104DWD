3917fc81a0806fa1b4c020d98be4a29e824896cd
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-10 22:22:17 +0100

    Adding code from slides to actually show red circles.

--- week6/lab12/main.js
+++ week6/lab12/main.js
@@ -15,5 +15,22 @@ $('#shakey').click(function() {
   $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(result) {
     console.log(result);
     //Logging the result from the JSON download to the console
-  })
+
+    result.features.forEach(function(quake) {
+    //For each earthquake
+        var lng = quake.geometry.coordinates[0];
+        var lat = quake.geometry.coordinates[1];
+        //Getting the co-ordinates
+
+        var circle = L.circle([lng,lat], 1, {
+            color: 'red',
+            opacity: 0,
+            fillColor: 'red',
+            fillOpacity: 0.8
+        })
+        //Creating a circle at that point, styling it
+        circle.addTo(mymap);
+        //Adding it to the map
+    });
+  });
 })
