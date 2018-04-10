ce7b642f6a21cae65b7b397f8e9a62a3b2173257
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-10 19:14:08 +0100

    Adding the code from the slide.

--- week6/addItem.js
+++ week6/addItem.js
@@ -2,4 +2,17 @@ $(function (){
 //Function will run when the page is loaded
     alert("Document is ready.");
     //The document is ready
+
+    S('#searchform').submit(function() {
+    //Tying a listener to the submit event of the form button
+      addItemToList("Example Item");
+      //Calling the function with a String parameter
+      return false;
+      //Preventing the page from reloading
+    });
 });
+
+function addItemToList(item) {
+  $('#results').append("<li>" + item + "</li>");
+  //Adding the "item" to the end of our list
+}
