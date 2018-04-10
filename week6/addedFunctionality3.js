e93368d5aeea789744141d841e2d47f7874e2155
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-10 21:14:43 +0100

    Adding parameter looking for movie only

--- week6/addedFunctionality3.js
+++ week6/addedFunctionality3.js
@@ -15,8 +15,8 @@ $(function (){
 });
 
 function getResultsFromOMDb(searchTerms) {
-  var url = "http://www.omdbapi.com/?i=tt3896198&apikey=32c3c53&s=" + searchTerms;
-  //Building the URL for the request
+  var url = "http://www.omdbapi.com/?i=tt3896198&apikey=32c3c53&s=" + searchTerms + "&t=movie";
+  //Building the URL for the request - looking for movies only
   $.getJSON(url, function(jsondata) {
   //Using the JQuery JSON shortcut
       addResults(jsondata);
