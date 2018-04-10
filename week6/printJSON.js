35d549ce583ee636c0106f10e6ade9a2055f0d57
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-10 19:37:56 +0100

    Adding the code from the slides to print the JSON.

--- week6/printJSON.js
+++ week6/printJSON.js
@@ -7,12 +7,29 @@ $(function (){
     //Tying a listener to the submit event of the form button
       var searchTerms = $('#searchterms').val();
       //Getting the item in the search term box
-      addItemToList(searchTerms);
+      getResultsFromOMDb(searchTerms);
       //Calling the function with the search terms as the parameter
       return false;
       //Preventing the page from reloading
     });
 });
+
+function getResultsFromOMDb(searchTerms) {
+  var url = "http://www.omdbapi.com/?i=tt3896198&apikey=8d5fad5a&s=" + searchTerms;
+  //Building the URL for the request
+  $.getJSON(url, function(jsondata) {
+  //Using the JQuery JSON shortcut
+      printJSON(jsondata);
+      //Handling the results
+  });
+}
+
+function printJSON(jsondata) {
+  var normal = JSON.stringify(jsondata);
+  //Converting the JSON data to a printable String
+  $('#resultsbox').append("<p>" + normal + "</p>");
+  //Printing the JSON to the screen
+}
 
 function addItemToList(item) {
   $('#results').append("<li>" + item + "</li>");
