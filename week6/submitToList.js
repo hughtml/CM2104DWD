ba0ca4acc5f600458fc2293ebaee7f92ae98ba59
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-10 19:19:27 +0100

    Creating the new script file.

---
+++ week6/submitToList.js
@@ -0,0 +1,18 @@
+$(function (){
+//Function will run when the page is loaded
+    alert("Document is ready.");
+    //The document is ready
+
+    $('#searchform').submit(function() {
+    //Tying a listener to the submit event of the form button
+      addItemToList("Example Item");
+      //Calling the function with a String parameter
+      return false;
+      //Preventing the page from reloading
+    });
+});
+
+function addItemToList(item) {
+  $('#results').append("<li>" + item + "</li>");
+  //Adding the "item" to the end of our list
+}
