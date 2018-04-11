ccdccbdf4ce698ab41677f99a68eae08c5cf030d
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-11 21:36:24 +0100

    Adding to comments.

--- week7/lab13/myfirstserver.js
+++ week7/lab13/myfirstserver.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function(req, res) {
-//Creating a server
+//Creating a server with the request to and response from the server
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
     //Responding with "Hello World"
