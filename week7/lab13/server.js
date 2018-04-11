b42e1c9d7cc0e38985c8044e063f6e42a85b2fbc
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-11 21:45:41 +0100

    Attempting the knock knock jokes using Express.

--- week7/lab13/server.js
+++ week7/lab13/server.js
@@ -1,5 +1,7 @@
 //Implementing the first server, but much shorter
 var express = require('express');
+var knockknock = require('knock-knock-jokes');
+//Using the knock knock jokes module
 var app = express();
 
 //Route 1
@@ -12,6 +14,14 @@ app.get('/', function(req, res) {
 
 app.get('/test', function(req, res) {
   res.send("This is Route 2");
+});
+
+//Route 3
+
+app.get('/joke', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var randomJoke = knockknock();
+  res.end(randomJoke);
 });
 
 app.listen(8080);
