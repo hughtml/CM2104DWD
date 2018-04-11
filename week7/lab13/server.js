83627935c42f3deeb16ab63c333269ac6aeb9e0a
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-11 22:57:44 +0100

    Test

--- week7/lab13/server.js
+++ week7/lab13/server.js
@@ -41,6 +41,7 @@ app.get('/calc', function(req, res) {
   var url_parts = url.parse(req.url, true);
   //Storing the url and then parsing the result
   var query = url_parts.query;
+  console.log(query);
   var x = query[1];
   var y = query[3];
   console.log(x);
