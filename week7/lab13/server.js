a7576d1b32514a0e14d25a0a5e152f0966d33d9f
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-11 21:40:45 +0100

    Added route 2.

--- week7/lab13/server.js
+++ week7/lab13/server.js
@@ -2,11 +2,17 @@
 var express = require('express');
 var app = express();
 
+//Route 1
+
 app.get('/', function(req, res) {
   res.send("Hello world! - by express");
 });
 
+//Route 2
+
+app.get('/test', function(req, res) {
+  res.send("This is Route 2");
+});
+
 app.listen(8080);
 //Listening on port 8080
-
-//Route 1
