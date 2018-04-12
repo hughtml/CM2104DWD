58f9f4a214391eb7bab21c75b06b73e288e4d189
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-12 23:04:58 +0100

    Used . instead of ,.

--- week7/lab14/server.js
+++ week7/lab14/server.js
@@ -109,7 +109,7 @@ app.get('/searchterm', function(req, res) {
 app.get('/tweetsjson', function(req, res) {
   var params = {screen_name: 'nodejs'};
   //Setting up the parameters
-  client.get('statuses/user_timeline', params. function(error, tweets, response) {
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
   //Telling the module what part of Twitter we want to search i.e. timeline
     if (!error) {
       var json = [];
