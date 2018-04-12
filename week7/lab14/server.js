ea7ff06fee7fa1e9182303ff15edfc9646175435
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-12 09:42:34 +0100

    Adding the new code for the lab14 server.js file.

--- week7/lab14/server.js
+++ week7/lab14/server.js
@@ -1,70 +1,16 @@
-//Implementing the first server, but much shorter
 var express = require('express');
-//Using the knock knock jokes module
+//Setting up express
 var app = express();
-
+//And setting up the app
 app.use(express.static('public'));
-//Looking in the public folder
+//Telling express to look in the public folder
 
-//Route 1
+//Default route
 
 app.get('/', function(req, res) {
-  res.send("Hello world! - by express");
-});
-
-//Route 2
-
-app.get('/test', function(req, res) {
-  res.send("This is Route 2");
-});
-
-//Route 3
-
-app.get('/add', function(req, res) {
-  var x = parseInt(req.query.x);
-  var y = parseInt(req.query.y);
-  //Accessing the values of x and y sent by the client
-  res.send("x + y = " + (x + y));
-  //Displaying the sum of both
-});
-
-//Route 4
-
-app.get('/calc', function(req, res) {
-  var url = require('url');
-  var url_parts = url.parse(req.url, true);
-  //Storing the url and then parsing the result
-  var query = url_parts.query;
-  var x = parseInt(query.x);
-  var y = parseInt(query.y);
-  //Accessing the values of x and y sent by the client
-  var operation = Object.getOwnPropertyNames(query)[2];
-  console.log(operation);
-  //Accessing the operation sent by the client
-  if (operation === "add") {
-    res.send("x + y = " + (x + y));
-    //Displaying the sum of both
-  } else if (operation === "sub") {
-    res.send("x - y = " + (x - y));
-    //Displaying the subtraction of both
-  } else if (operation === "mul") {
-    res.send("x * y = " + (x * y));
-    //Displaying the multiplication of both
-  } else if (operation === "div") {
-    res.send("x / y = " + (x / y));
-    //Displaying the division of both
-  }
-});
-
-//Route 5
-
-app.get('/getform', function(req, res) {
-  var name = req.query.name;
-  var quest = req.query.quest;
-  //Accessing the values of name and quest sent by the client
-  res.send("Hi " + name + ", I am sure you will " + quest);
-  //Displaying the sum of both
+  res.send("Hello world! by express");
+  //Displaying a message
 });
 
 app.listen(8080);
-//Listening on port 8080
+//Telling the app to listen on port 8080
