e7f9f8e72b96d5cfe3c3374c9231df42837951f4
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-12 09:56:45 +0100

    Forgot to encase keys in parenthesis.

--- week7/lab14/server.js
+++ week7/lab14/server.js
@@ -6,10 +6,10 @@ var Twitter = require('twitter');
 //Importing the Twitter module
 
 var client = new Twitter({
-  consumer_key: zRXeWUS9KpYktPvSAipW1jLbb,
-  consumer_secret: Tn7sakGVCtfSDtodmYH7QrIuuES74O8og35rxdaERvpha0817T,
-  access_token_key: NUrzzHnyadmAC70KM8HnXUOXxowAd54JUHT5cNnS,
-  access_token_secret: lgx0mNh4uICekIN9egMqNHgbeWGR1hyJ3GK8JK0QoDJWR
+  consumer_key: 'zRXeWUS9KpYktPvSAipW1jLbb',
+  consumer_secret: 'Tn7sakGVCtfSDtodmYH7QrIuuES74O8og35rxdaERvpha0817T',
+  access_token_key: '354524237-NUrzzHnyadmAC70KM8HnXUOXxowAd54JUHT5cNnS',
+  access_token_secret: 'lgx0mNh4uICekIN9egMqNHgbeWGR1hyJ3GK8JK0QoDJWR'
 });
 //Filling in details for the client object so Twitter will work
 
