a9f239d35b216c84992368cb92e30678bacf0984
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-12 09:40:01 +0100

    Deleting the old files from lab13.

--- week7/lab14/public/myfirstserver.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res) {
-//Creating a server with the request to and response from the server
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-    //Responding with "Hello World"
-}).listen(8080);
-//Listening on port 8080
--- week7/lab14/public/mymodule.js
+++
@@ -1,6 +0,0 @@
-exports.myDateTime = function() {
-//Creating the function using keyword exports
-//This means other code can use it
-  return Date();
-  //Returning the current date
-};
