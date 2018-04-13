d5f1fe17ac56d9cab2fe3bce022d1f4ce6dece12
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-13 21:00:46 +0100

    Test.

--- week9/lab18/server.js
+++ week9/lab18/server.js
@@ -115,11 +115,7 @@ app.post('/dologin', function(req, res) {
     if(!result){res.redirect('/login');return}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
     if(result.login.password == pword){ req.session.loggedin = true;
-      db.collection('people').findOne({"login.username":uname}, function(err2, result2) {
-        if (err2) throw err2;//if there is an error, throw the error
-        //if there is no result, redirect the user back to the login system as that username must not exist
-        res.render('/', {uname: result2});
-      });
+      res.redirect('/');
     //otherwise send them back to login
     } else {res.redirect('/login')}
   });
